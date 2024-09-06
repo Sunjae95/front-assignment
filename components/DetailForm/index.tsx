7239d3f8f04669cf534ebe715640fad1cd0b2f8c
SeonjaeLee <tjswo9502@gmail.com>
2024-09-07 04:41:04 +0900

    ♻️ detailForm Dom 구조변경 및 함수 리팩토링

--- components/DetailForm/index.tsx
+++ components/DetailForm/index.tsx
@@ -32,118 +32,125 @@ export const DetailForm = ({
   const [isOpen, setIsOpen] = useState(false);
   const [form, setForm] = useState({ isSuccess, title, content });
 
-  const handleOnDelete = async () => {
-    const data = new FormData();
-    data.append("id", params.id);
-    await onDelete(data);
-  };
+  const handleOnChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => setForm((f) => ({ ...f, [e.target.name]: e.target.value }));
+
+  const handleOnClickEditPrevious = useCallback(() => {
+    if (title !== form.title || content !== form.content) {
+      setIsOpen(true);
+    } else {
+      push(pathname);
+    }
+  }, [push, pathname]);
+
+  const handleOnClickAlert = useCallback(() => {
+    if (mode === "edit") {
+      setIsOpen(false);
+      push(pathname);
+    } else {
+      const data = new FormData();
+      data.append("id", params.id);
+      onDelete(data);
+    }
+  }, [params, push, pathname, onDelete]);
 
   const handleOnUpdate = useCallback(
-    async (data: FormData) => {
+    (data: FormData) => {
       data.append("id", params.id);
       data.append("isSuccess", isSuccess.toString());
-      await onUpdate(data);
+      onUpdate(data);
     },
     [push, pathname, onUpdate]
   );
 
   const handleOnChangeCheckbox = useCallback(
-    async (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: React.ChangeEvent<HTMLInputElement>) => {
       setForm((f) => ({ ...f, isSuccess: e.target.checked }));
       const data = new FormData();
       data.append("id", params.id);
       data.append("isSuccess", e.target.checked.toString());
       data.append("title", form.title);
       data.append("content", form.content);
-      await onUpdate(data);
+      onUpdate(data);
     },
     [form.title, form.content, onUpdate]
   );
 
-  if (mode === "read") {
-    return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <Checkbox
-            label={title}
-            checked={form.isSuccess}
-            onChange={handleOnChangeCheckbox}
-          />
-          <div className={styles["button-wrapper"]}>
-            <Button
-              color="black"
-              width={"80px"}
-              onClick={() => push("?mode=edit")}
-            >
-              Update
-            </Button>
-            <Button
-              theme="dangerous"
-              color="red"
-              width={"80px"}
-              onClick={() => setIsOpen(true)}
-            >
-              Delete
-            </Button>
-          </div>
-        </div>
-        <p className={styles.content}>{content}</p>
-        <div className={styles["button-wrapper"]}>
-          <Button color="black" width={"80px"} onClick={() => push("/")}>
-            Previous
-          </Button>
-        </div>
-        {isOpen && (
-          <Alert
-            content="todo를 정말 삭제할까요?"
-            left={{
-              color: "black",
-              children: "Cancel",
-              onClick: () => setIsOpen(false),
-            }}
-            right={{
-              theme: "dangerous",
-              color: "red",
-              children: "Ok",
-              onClick: handleOnDelete,
-            }}
-          />
-        )}
-      </div>
-    );
-  }
-
   return (
     <form className={styles.container} action={handleOnUpdate}>
       <div className={styles.header}>
-        <input
-          className={styles.input}
-          name="title"
-          value={form.title}
-          onChange={(e) =>
-            setForm((f) => ({ ...f, [e.target.name]: e.target.value }))
-          }
-        />
+        {mode === "edit" ? (
+          <input
+            className={styles.input}
+            name="title"
+            value={form.title}
+            onChange={handleOnChange}
+          />
+        ) : (
+          <>
+            <Checkbox
+              label={title}
+              checked={form.isSuccess}
+              onChange={handleOnChangeCheckbox}
+            />
+            <div className={styles["button-wrapper"]}>
+              <Button
+                color="black"
+                width={"80px"}
+                onClick={() => push("?mode=edit")}
+              >
+                Update
+              </Button>
+              <Button
+                theme="dangerous"
+                color="red"
+                width={"80px"}
+                onClick={() => setIsOpen(true)}
+              >
+                Delete
+              </Button>
+            </div>
+          </>
+        )}
       </div>
-      <textarea
-        className={styles.content}
-        name="content"
-        value={form.content}
-        onChange={(e) =>
-          setForm((f) => ({ ...f, [e.target.name]: e.target.value }))
-        }
-      />
+      {mode === "edit" ? (
+        <textarea
+          className={styles.content}
+          name="content"
+          value={form.content}
+          onChange={handleOnChange}
+        />
+      ) : (
+        <p className={styles.content}>{content}</p>
+      )}
       <div className={styles["button-wrapper"]}>
-        <Button type="submit" theme="dangerous" color="red" width={"80px"}>
-          Ok
-        </Button>
-        <Button color="black" width={"80px"} onClick={() => setIsOpen(true)}>
-          Previous
-        </Button>
+        {mode === "edit" ? (
+          <>
+            <Button type="submit" theme="dangerous" color="red" width={"80px"}>
+              Ok
+            </Button>
+            <Button
+              color="black"
+              width={"80px"}
+              onClick={handleOnClickEditPrevious}
+            >
+              Previous
+            </Button>
+          </>
+        ) : (
+          <Button color="black" width={"80px"} onClick={() => push("/")}>
+            Previous
+          </Button>
+        )}
       </div>
       {isOpen && (
         <Alert
-          content="변경사항이 있습니다. 수정을 취소할까요?"
+          content={
+            mode === "edit"
+              ? "변경사항이 있습니다. 수정을 취소할까요?"
+              : "todo를 정말 삭제할까요?"
+          }
           left={{
             color: "black",
             children: "Cancel",
@@ -153,10 +160,7 @@ export const DetailForm = ({
             theme: "dangerous",
             color: "red",
             children: "Ok",
-            onClick: () => {
-              setIsOpen(false);
-              push(pathname);
-            },
+            onClick: handleOnClickAlert,
           }}
         />
       )}
