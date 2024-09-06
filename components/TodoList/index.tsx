96923c0eefefb809af9d1c83258150773f8452cb
SeonjaeLee <tjswo9502@gmail.com>
2024-09-07 06:51:12 +0900

    💄 워딩스타일 수정

--- components/TodoList/index.tsx
+++ components/TodoList/index.tsx
@@ -99,8 +99,8 @@ export const TodoList = ({
     <>
       <div className={styles["create-wrapper"]}>
         <Button
-          theme="primary"
-          color="black"
+          theme="dangerous"
+          color="green"
           width={"70px"}
           onClick={() => setForm(initialForm)}
         >
